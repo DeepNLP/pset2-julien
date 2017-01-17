Fix softmax to work with q1_classifier (None dimensions)

q1_softmax.py
@@ -24,7 +24,9 @@ def softmax(x):
   ### YOUR CODE HERE
   x_norm = x - tf.reduce_max(x, axis=1, keep_dims=True)
   exp = tf.exp(x_norm)
-  out = tf.div(exp, tf.reduce_sum(exp, axis=1))
+  denominator = tf.reduce_sum(exp, axis=1)
+  denominator = tf.reshape(denominator, [-1, 1])
+  out = tf.div(exp, denominator)
   ### END YOUR CODE
   
   return out 