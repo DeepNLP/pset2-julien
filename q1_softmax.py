0dda5146b6593b8fc66161631988ec5622c71937
Julien Chaumond <chaumond@gmail.com>
2017-01-17 18:41:30 +0100

    softmax

--- q1_softmax.py
+++ q1_softmax.py
@@ -22,7 +22,9 @@ def softmax(x):
   """
 
   ### YOUR CODE HERE
-  raise NotImplementedError
+  x_norm = x - tf.reduce_max(x, axis=1, keep_dims=True)
+  exp = tf.exp(x_norm)
+  out = tf.div(exp, tf.reduce_sum(exp, axis=1))
   ### END YOUR CODE
   
   return out 
